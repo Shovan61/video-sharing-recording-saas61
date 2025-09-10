975d61407c84aee622cd2f4a844180ac375fe366
Shovan61 <shovonmazumder61@gmail.com>
2025-09-10 10:39:48 +0600

    updated

--- electron-desktop-app-for-recording-video/src/App.tsx
+++ electron-desktop-app-for-recording-video/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-	return (
-		<>
-			<Button variant={"default"}>Shadcn</Button>
-		</>
-	);
+	return <QueryClientProvider client={queryClient}>
+		<span className="text-green-700 text-4xl font-bold">Desktop</span>
+	</QueryClientProvider>;
 }
 
 export default App;
